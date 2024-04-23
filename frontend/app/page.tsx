cba0ef90f0c768e187f7adff86b29b056c3ef68a
Sanan bin Tahir <sananbintahir@gmail.com>
2024-04-23 16:03:06 +0500

    fix import error

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,8 @@
 import { getServerSession } from "next-auth/next";
 import ChatSection from "./components/chat-section";
-import { authOptions } from "./api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import Header from "./components/header";
+import { authOptions } from "@/auth";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
